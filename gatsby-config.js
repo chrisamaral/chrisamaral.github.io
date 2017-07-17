120e6c2aaeb92e0529617798ca4b6fb7ea0d541d
Christian Amaral <darthcas@gmail.com>
2017-07-17 03:05:46 -0300

    come into terms with graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,8 @@
 module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet'
-  ]
+  ],
+  siteMetadata: {
+    title: 'chris@maral'
+  }
 }
--- src/Page.js
+++ src/Page.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Head from './Head'
+import {Helmet} from 'react-helmet'
 
-const Page = ({children}) => (
+require('tachyons/css/tachyons.css')
+
+const Page = ({children, site}) => (
   <div className='code'>
     {children}
-    <Head />
+
+    <Helmet>
+      <title>{site.siteMetadata.title}</title>
+    </Helmet>
   </div>
 )
 
 Page.displayName = 'Page'
 Page.propTypes = {
+  site: PropTypes.object.isRequired,
   children: PropTypes.node.isRequired
 }
 
